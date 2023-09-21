20fe737a3c10c7b8f308c2f64dafa82d6da93f86
ohapon <ohapon@gmail.com>
2023-09-21 14:45:49 +0300

    Add str functions

--- src/main/python/plazma/kernel/lib/str/strlib.py
+++ src/main/python/plazma/kernel/lib/str/strlib.py
@@ -4,131 +4,157 @@ BLANK_STRING = ""
 #### 1.1
 
 def isEmpty(str, trim = False):
-    if (not trim):
-        return _isEmpty(str)
-    return _isEmpty(str.strip())
+    if (str == None):
+        return True
 
-def _isEmpty(str):
-    return size(str) == 0
+    if (not trim):
+        return len(str) == 0
+    
+    return len(str.strip()) == 0 # trim
+
+def isBlank(str):
+    if (str == None):
+        return True
+    
+    return len(str.strip()) == 0 # trim
 
 def size(str):
+    if (str == None):
+        return 0
     return len(str)
 
+def equals(str1, str2):
+    if (str1 == None or str2 == None):
+        return False    
+    return str1 == str2    
+
 #### 1.2
+
+def normalize(str):
+    if (str == None):
+        return None
+    return str.strip() # trim
+
+def normalizeBlank(str, trimAll, trimBlank):
+    if (str == None):
+        return None
+
+    if (len(str) == 0):
+        return BLANK_STRING
+    
+    if (not trimAll and not trimBlank):
+        return str
+    
+    if (not trimAll):
+        # trimBlank=True, because we have condition (not trimAll and not trimBlank) before
+        # Analize blank
+        if (isBlank(str)):
+            return BLANK_STRING
+        else:
+            return str
+    else:
+        return normalize(str)
+
+# None: experimental
+def emptyIfNone(str):
+    if (str == None):
+        return BLANK_STRING
+    else:
+        return str
+
+def noneIfEmpty(str):
+    if (str == None or len(str) == 0):
+        return None
+    else:
+        return str
+
+def defaultIfNone(str, defaultStr):
+    if (str == None):
+        return defaultStr
+    else:
+        return str
+
+def defaultIfEmpty(str, defaultStr):
+    if (str == None or len(str) == 0):
+        return defaultStr
+    else:
+        return str
+
+# Null: experimental
+def emptyIfNull(str):
+    return emptyIfNone(str);
+
+def nullIfEmpty(str):
+    return noneIfEmpty(str)
+
+def defaultIfNull(str, defaultStr):
+    return defaultIfNone(str, defaultStr)
 
 #### 1.3
 
 #### 2.1
 
 def replicate(str, n):
-    if (n < 1):
-        BLANK_STRING
+    if (str == None):
+        return None
     return str * n
 
 ## trim
 
 def trim(str, ch = None):
-    if (ch == None):
-        return str.strip()
-
-    len = size(str)
-    if (len == 0):
-        return str
-    
-    # find left position of last 'ch'
-    posFirst = findFirstNotOf(str, ch, len)
-
-    # not found
-    if (posFirst < 0):
-        return str
-
-    # all chars are 'ch'
-    if (posFirst == len - 1):
-        return ""
-  
-    posFirst += 1
-
-    # find right position of first 'ch'
-    posLast = findLastNotOf(str, ch, len)
-
-    if (posLast < 0):
-        return str[posFirst:]
-
-    return str[posFirst:posLast]
-
+    if (str == None):
+        return None
+    return str.strip(ch)
 
 def ltrim(str, ch = None):
-    if (ch == None):
-        return str.lstrip()
-
-    len = size(str)
-    if (len == 0):
-        return str
-    
-    # find left position of last 'ch'
-    pos = findFirstNotOf(str, ch, len)
-
-    # not found 'ch'
-    if (pos < 0):
-        return str
-
-    # all chars are 'ch'
-    if (pos == len - 1):
-        return ""
-
-    pos += 1
-    return str[pos:]
-
+    if (str == None):
+        return None
+    return str.lstrip(ch)
 
 def rtrim(str, ch = None):
-    if (ch == None):
-        return str.rstrip()
+    if (str == None):
+        return None
+    return str.rstrip(ch)
+
+def findFirstNotOf(str, ch, start = None):
+    if (str == None):
+        return None
+
+    if (start == None):
+        start = 0
 
     len = size(str)
     if (len == 0):
-        return str
-    
-    # find right position of first 'ch'
-    pos = findLastNotOf(str, ch, len)
-
-    # not found 'ch'
-    if (pos < 0):
-        return str
-
-    return str[0:b]
-
-
-def findFirstNotOf(str, ch, len = None):
-    if (len == None):
-        len = size(str)
-
-    if (len == 0):
         return -1
     
-    # find left position of last 'ch'
-    p = -1
-    i = 0
-    while (i < len):
+    if (start < 0 or start > len - 1):
+        return -1
+
+    i = start
+    while (i < len):        
+        if (str[i] != ch):
+            return i
         i += 1
-        if (str[i] != ch):
-            break
-        p = i
-    return p
+    return -1
 
-def findLastNotOf(str, ch, len = None):
-    if (len == None):
-        len = size(str)
+def findLastNotOf(str, ch, end = None):
+    if (str == None):
+        return None
 
+    len = size(str)
     if (len == 0):
         return -1
-    
-    # find right position of first 'ch'
-    p = -1
-    i = len
-    while (i >= 0):
-        i -= 1
+
+    if (end == None):
+        end = len - 1
+
+    if (end < 0 or end > len - 1):
+        return -1
+
+    i = end
+    while (i >= 0):        
         if (str[i] != ch):
-            break
-        p = i
-    return p
+            return i
+        i -= 1
+    return -1
 
