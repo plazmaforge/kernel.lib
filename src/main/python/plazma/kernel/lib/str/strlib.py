eadcb0be966dae990a017379221178fce3f8c286
ohapon <ohapon@gmail.com>
2023-09-23 17:32:31 +0300

    Refactored

--- src/main/python/plazma/kernel/lib/str/strlib.py
+++ src/main/python/plazma/kernel/lib/str/strlib.py
@@ -1,56 +1,71 @@
 
-EMPTY_STRING = ""
-BLANK_STRING = " "
-ELLIPSIS = ".."
+EMPTY_STRING = ''
+SPACE_CHAR = ' '
+ELLIPSIS = '...'
 ELLIPSIS_LEN = 3
 
 #### 1.1
 
 def isEmpty(str):
-    if (str == None):
+    if str == None:
         return True    
     return len(str) == 0
 
 
 def isBlank(str):
-    if (str == None):
+    if str == None:
         return True    
     return len(str.strip()) == 0 # trim
 
 
 def size(str):
-    if (str == None):
+    if str == None:
         return 0
     return len(str)
 
 
 def equals(str1, str2):
-    if (str1 == None or str2 == None):
+    if str1 == None or str2 == None:
         return False    
     return str1 == str2    
 
 #### 1.2
 
 def normalize(str):
-    if (str == None):
+    if str == None:
         return None
+    
+    if len(str) == 0:
+        return None
+
+    s = str.strip() # trim
+
+    if len(s) == 0:
+        return None
+    else:
+        return s
+
+
+def normalizeSafe(str):
+    if str == None:
+        return EMPTY_STRING    
     return str.strip() # trim
 
 
 def normalizeBlank(str, trimAll, trimBlank):
-    if (str == None):
+    if str == None:
         return None
 
-    if (len(str) == 0):
-        return EMPTY_STRING
+    #if len(str) == 0:
+    #    return EMPTY_STRING
     
-    if (not trimAll and not trimBlank):
+    if not trimAll and not trimBlank:
         return str
     
-    if (not trimAll):
+    if not trimAll:
         # trimBlank=True, because we have condition (not trimAll and not trimBlank) before
         # Analize blank
-        if (isBlank(str)):
+        if isBlank(str):
             return EMPTY_STRING
         else:
             return str
@@ -60,28 +75,28 @@ def normalizeBlank(str, trimAll, trimBlank):
 
 # None: experimental
 def emptyIfNone(str):
-    if (str == None):
+    if str == None:
         return EMPTY_STRING
     else:
         return str
 
 
 def noneIfEmpty(str):
-    if (str == None or len(str) == 0):
+    if str == None or len(str) == 0:
         return None
     else:
         return str
 
 
 def defaultIfNone(str, defaultStr):
-    if (str == None):
+    if str == None:
         return defaultStr
     else:
         return str
 
 
 def defaultIfEmpty(str, defaultStr):
-    if (str == None or len(str) == 0):
+    if str == None or len(str) == 0:
         return defaultStr
     else:
         return str
@@ -104,40 +119,40 @@ def defaultIfNull(str, defaultStr):
 ## trim
 
 def trim(str, ch = None):
-    if (str == None):
+    if str == None:
         return None
     return str.strip(ch)
 
 
 def ltrim(str, ch = None):
-    if (str == None):
+    if str == None:
         return None
     return str.lstrip(ch)
 
 
 def rtrim(str, ch = None):
-    if (str == None):
+    if str == None:
         return None
     return str.rstrip(ch)
 
 #### 1.4
 
 def findFirstNotOf(str, ch, start = None):
-    if (str == None):
+    if str == None:
         return None
 
-    if (start == None):
+    if start == None:
         start = 0
 
     len = size(str)
-    if (len == 0):
+    if len == 0:
         return -1
     
-    if (start < 0 or start > len - 1):
+    if start < 0 or start > len - 1:
         return -1
 
     i = start
-    while (i < len):        
+    while (i < len):
         if (str[i] != ch):
             return i
         i += 1
@@ -145,21 +160,21 @@ def findFirstNotOf(str, ch, start = None):
 
 
 def findLastNotOf(str, ch, end = None):
-    if (str == None):
+    if str == None:
         return None
 
     len = size(str)
-    if (len == 0):
+    if len == 0:
         return -1
 
-    if (end == None):
+    if end == None:
         end = len - 1
 
-    if (end < 0 or end > len - 1):
+    if end < 0 or end > len - 1:
         return -1
 
     i = end
-    while (i >= 0):        
+    while (i >= 0):
         if (str[i] != ch):
             return i
         i -= 1
@@ -169,10 +184,10 @@ def findLastNotOf(str, ch, end = None):
 #### 2.1
 
 def replicate(str, n):
-    if (str == None):
+    if str == None:
         return None
     
-    if (n < 1):
+    if n < 1:
         return str
 
     return str * n
@@ -180,78 +195,78 @@ def replicate(str, n):
 
 #### 2.2
 
-def lpad(str, len, pad = " "):
-    if (str == None):
+def lpad(str, len, pad = ' '):
+    if str == None:
         return None
     
-    if (len < 1 or pad == None):      # pad == None: no padding
+    if len < 1 or pad == None:       # pad == None: no padding
         return str
         
     padLen = size(pad)
-    if (padLen == 1):
-        return str.rjust(len, pad)     # lpad = rjust for padlen = 1
+    if padLen == 1:
+        return str.rjust(len, pad)   # lpad = rjust for padlen = 1
     
     strLen = size(str)
-    if (len <= strLen or padLen == 0): # padLen == 0: no padding
+    if len <= strLen or padLen == 0: # padLen == 0: no padding
         return str
     
     fillLen = len - strLen
     padCount = fillLen // padLen
 
-    if (fillLen % padLen > 0):
+    if fillLen % padLen > 0:
         padCount = padCount + 1
 
     fillStr = replicate(pad, padCount)
 
-    if (size(fillStr) > fillLen):
+    if size(fillStr) > fillLen:
         return fillStr[:fillLen] + str
     else:
         return fillStr + str
 
 
-def rpad(str, len, pad = " "):
-    if (str == None):
+def rpad(str, len, pad = ' '):
+    if str == None:
         return None
     
-    if (len < 1 or pad == None):      # pad == None: no padding
+    if len < 1 or pad == None:       # pad == None: no padding
         return str
         
     padLen = size(pad)
-    if (padLen == 1):
-        return str.ljust(len, pad)     # rpad = ljust for padlen = 1
+    if padLen == 1:
+        return str.ljust(len, pad)   # rpad = ljust for padlen = 1
     
     strLen = size(str)
-    if (len <= strLen or padLen == 0): # padLen == 0: no padding
+    if len <= strLen or padLen == 0: # padLen == 0: no padding
         return str
     
     fillLen = len - strLen
     padCount = fillLen // padLen
 
-    if (fillLen % padLen > 0):
+    if fillLen % padLen > 0:
         padCount = padCount + 1
 
     fillStr = replicate(pad, padCount)
 
-    if (size(fillStr) > fillLen):
+    if size(fillStr) > fillLen:
         return str + fillStr[:fillLen]
     else:
         return str + fillStr
 
 
-def fill(str, len, pad = " "):
-    if (str == None or len < 1):
+def fill(str, len, pad = ' '):
+    if str == None or len < 1:
         return EMPTY_STRING
     
     strLen = size(str)
-    if (strLen == len):
+    if strLen == len:
         return str
     
-    if (strLen < len):
+    if strLen < len:
         # add <pad> to right side
         return rpad(str, len, pad)
     else:
         # remove chars from right side
-        return trunc(str, len, True, True)
+        return trunc(str, len, False, True)
 
 
 def ellipsis(str, len):
@@ -259,20 +274,20 @@ def ellipsis(str, len):
 
 
 def trunc(str, len, trim = False, ellipsis = False):
-    if (str == None):
+    if str == None:
         return None
     
-    if (len < 1):
+    if len < 1:
         return str
     
-    if (trim):
+    if trim:
         str = str.strip() # trim
 
-    if (size(str) <= len):
+    if size(str) <= len:
         return str
     
-    if (ellipsis):
-        if (len <= ELLIPSIS_LEN):
+    if ellipsis:
+        if len <= ELLIPSIS_LEN:
             return str[:len]
         else:
             return str[:len - ELLIPSIS_LEN] + ELLIPSIS
@@ -281,22 +296,22 @@ def trunc(str, len, trim = False, ellipsis = False):
     
 
 def left(str, len):
-    if (str == None):
+    if str == None:
         return None
         
     strLen = size(str)
-    if (strLen <= len):
+    if strLen <= len:
         return str
     else:    
         return str[:len]
 
 
 def right(str, len):
-    if (str == None):
+    if str == None:
         return None
         
     strLen = size(str)
-    if (strLen <= len):
+    if strLen <= len:
         return str
     else:    
         return str[strLen - len:]
