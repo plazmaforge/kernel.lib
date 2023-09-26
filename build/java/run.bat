d3380b095e6bc3e0c8c80f4892de87093d103c60
ohapon <ohapon@gmail.com>
2023-09-26 16:19:55 +0300

    Reorganize packages

--- build/java/run.bat
+++ build/java/run.bat
@@ -1,7 +1,7 @@
 
 set "APP_PATH=%~dp0..\.."
 set "LIB_PATH=%APP_PATH%\target"
-set "CP=%LIB_PATH%\plazma-kernel-lib-1.0.8.jar"
+set "CP=%LIB_PATH%\plazma-lib-1.0.8.jar"
 
 rem -Xmx64m
 java -cp %CP% plazma.lib.task.Run %*
--- build/java/run.sh
+++ build/java/run.sh
@@ -3,7 +3,7 @@ APP_PATH=$PWD
 LIB_PATH=`dirname "$APP_PATH"`/..
 LIB_PATH=`cd "$LIB_PATH" && pwd`
 LIB_PATH=$LIB_PATH/target
-CP=$LIB_PATH/plazma-kernel-lib-1.0.8.jar
+CP=$LIB_PATH/plazma-lib-1.0.8.jar
 
 #-Xmx64m
 java -cp $CP plazma.lib.task.Run "$@"
